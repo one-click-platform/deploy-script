bc35cfdd815153233caff0842b7ad1c9c8c90254
xxxvik-xakerxxx <wolfis_rain@mail.ru>
2021-03-28 06:07:34 +0300

    run update front eth

--- keys/start_front.sh
+++ keys/start_front.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 su
 
-docker run -v /home/ubuntu/env.js:/usr/share/nginx/html/static/env.js -p 81:80 -d $1
+docker login docker.pkg.github.com --username xxxvik-xakerxxx --password 4ac0950429eec63b5c72306a43fc41090af6e720
+
+docker pull docker.pkg.github.com/one-click-platform/web-client/web_eth:latest
+
+docker run -v /home/ubuntu/env.js:/usr/share/nginx/html/static/env.js -p 81:80 -d docker.pkg.github.com/one-click-platform/web-client/web_eth
 
 systemctl restart nginx.service
 
